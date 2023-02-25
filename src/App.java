1bf5d387fac1a6787a03de60808f684381dc4c26
Elrak01 <61857848+Elrak01@users.noreply.github.com>
2023-02-24 22:34:56 -0800

    readme

--- src/App.java
+++ src/App.java
@@ -1,10 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        boolean[][] adjacencyMatrix = {
-            {false, true, true},
-            {false, false, true},
-            {false, false, false}
-        };
         Graph graph = new Graph();
         Display d = new Display("Graph Traversal", graph);
     }
